1a2dbcedb4d71cea81b6ea0b3f32bd9c3d2972e5
Raff2015 <erc0117@gmail.com>
2019-03-26 17:40:17 -0400

    lol

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Define UI Vars
-const form  = document.querySelector('#task-form');
-const taskList = document.querySelector('.collections');
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -9,37 +9,79 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 // Load all event listeners
-function loadEventListeners(){
-    // add task event
-    form.addEventListener('submit', addTask);
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
-function addTask(e){
-    if(taskInput.value === ''){
-        alert('Add Task');
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+    return false;
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
     }
+  }
+}
 
-    // Create li element
-    const li = document.createElement('li');
-    // Add Class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content'; //secondary-content to move item to the right using materlize.css
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append the link to li
-    li.appendChild(link);
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
 
-    // Append li to ul
-    taskList.appendChild(li);
-    
-    // Clear input
-    taskInput.value = '';
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild); // This is faster + better
+  }
 
-    e.preventDefault();
+  // https://jsperf.com/innerhtml-vs-removechild // Link that shows the difference with while & innerHTML
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){ //forEach to loop to search the tasks
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
